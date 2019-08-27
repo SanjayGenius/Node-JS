6a1b1d6c9fdc23bd2d069edeb7f142b6a6a37128
sanjay <sanjaym0621@gmail.com>
2019-08-27 13:39:38 +0530

    Report changes done

--- service/report.js
+++ service/report.js
@@ -3,35 +3,53 @@ var methods={
     getReport : function(req){
         var fromDate=req.query.fromDate;
         var toDate=req.query.toDate;
-        var customerName='';
         var customerArray=[];
-        
-        return dao.getBillReport(fromDate,toDate).then(function(response){
+        var finalData='';
+        return dao.getBillReport(fromDate,toDate).then(async function(response){
+            console.log(response.length)
+            if(response.length>0){
             for(var data in response){
-                var resultArray=[];
-               var customerObj='';
-              // module.exports.getDetailedReport(response[data].customer_id);
-            dao.getDetailedReport(response[data].customer_id).then(function(result){
-                  for(var resultData in result){
-                    var obj={
-                        "quantity" : result[resultData].quantity,
-                        "productName":result[resultData].product_name,
-                        "price" : result[resultData].price
-                    }
-                    resultArray.push(obj);
-                  }
-                  console.log(resultArray)
-                })
-                customerObj={
-                    "customerName": response[data].customer_name,
-                    "date" : response[data].visited_date,
-                    "detailedReport" : resultArray
-                }
-                customerArray.push(customerObj);
+               customerArray.push(await methods.getDetailedReport(response[data].customer_id,response[data].customer_name,response[data].visited_date,response[data].bill_amount)) 
             }
-            console.log(customerArray)
+            finalData={
+                "status" : "success",
+                "data" : customerArray,
+            }
+        }else{
+            finalData={
+                "status" : "failure",
+                "data" : "No reports available for the selected date range",
+            }
+        }
+            return finalData;
         })
         
+    },
+    getDetailedReport : function(cusId, customer_name, visited_date,billAmount){
+        let resultArray = [];
+        return dao.getDetailedReport(cusId).then(function(result){
+            for(var resultData in result){
+              var obj={
+                  "quantity" : result[resultData].quantity,
+                  "productName":result[resultData].product_name,
+                  "price" : result[resultData].price
+              }
+              resultArray.push(obj);
+            }
+            var year=visited_date.getFullYear();
+            var month= visited_date.getMonth()+1;
+            var day=visited_date.getDate();
+            var formatMonth=month>9?month:"0"+month;
+            var formatDay=day>9?day:"0"+day;
+            var formattedDate=year+"-"+formatMonth+"-"+formatDay;
+            customerObj={
+                "totalBillAmount" : billAmount,
+                "customerName": customer_name,
+                "date" : formattedDate,
+                "detailedReport" : resultArray
+          }
+          return customerObj;
+          })   
     }
 }
 exports.data=methods;
